6c6abb9fe411bf4455a83b42001ad97a21282acd
Gus <Gus@Michelles-MacBook-Pro.local>
2022-09-28 21:29:45 +0800

    made test, formatted with help of solution code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import renderer from 'react-test-renderer'
+//import renderer from 'react-test-renderer'
 
 
-test('renders a snapshot', () => {
-  const tree = renderer.create(<App/>).toJSON()
-  expect(tree).toMatchSnapshot()
+//test('renders a snapshot', () => {
+  //const tree = renderer.create(<App/>).toJSON()
+  //expect(tree).toMatchSnapshot()
+//})
+
+test('renders text Gustavo0623', () => {
+  render(<App />)
+  const me = screen.getByText(/Gustavo0623/i)
+  expect(me).toBeInTheDocument()
 })
-
